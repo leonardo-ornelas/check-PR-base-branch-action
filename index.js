47c70349101d38bbfcc07a12b8ea50c0cf3315fb
Leonardo de Santana Ornelas <leonardo.ornelas@gmail.com>
2021-07-17 23:36:00 -0300

    beta release

--- index.js
+++ index.js
@@ -12,61 +12,64 @@ const validEvent = ['pull_request'];
 const getSpecKey = (spec, branch) =>
     Object.keys(spec).find((target) => RegExp(target).test(branch));
 
-/**
- * 
- * @param {string} eventName Github event name
- * @param {string} baseBranch Destination branch 
- * @param {string} branch Current Branch
- * @returns 
- */
-async function run(eventName, baseBranch, branch) {
+
+async function run() {
     try {
 
-        core.info(`Event name: ${eventName}`);
+        const { eventName, payload: { pull_request } } = github.context
+
         if (validEvent.indexOf(eventName) < 0) {
             core.setFailed(`Invalid event: ${eventName}`);
             return;
         }
 
+        const baseBranch = pull_request.base.ref;
+        core.debug(`baseBranch: ${baseBranch}`);
+
         const ignore = core.getMultilineInput('ignore');
+        core.debug(`ignore : ${ignore}`);
         if (ignore.length > 0 && ignore.some((el) => baseBranch === el)) {
             core.info(`Skipping checks since ${baseBranch} is in the ignored list - ${ignore}`);
             return
         }
 
-        const spec = JSON.parse(core.getInput('spec'));
+        const specStr = core.getInput('spec');
+        core.debug(`spec: ${specStr}`);
 
-        var specKey = getSpecKey(spec, baseBranch);
+        const spec = JSON.parse(specStr);
 
-        if (specKey == null) {
-            core.info(`Skipping cheks since ${baseBranch} is not found`);
+        const branchKey = getSpecKey(spec, baseBranch);
+
+        if (branchKey == null) {
+            core.info(`Skipping checks since ${baseBranch} is not found`);
             return;
         }
 
+        const branch = pull_request.head.ref;
+        core.debug(`currentBranch: ${branch}`);
 
-        const specValor = spec[specKey];
+        const specBranch = spec[branchKey];
+        core.debug(`specBranch: ${specBranch}`);
 
         let isCorrect = false;
-        if (Array.isArray(specValor)) {
-            isCorrect = specValor.some(valor => branch.match(valor));
+        if (Array.isArray(specBranch)) {
+            isCorrect = specBranch.some(b => branch.match(b));
         } else {
-            isCorrect = specValor.match(valor);
+            isCorrect = branch.match(specBranch);
         }
 
 
-        if (isCorrect) {
-            core.setFailed(`Your branch is not allowed to PR into ${branch}`);
+        if (!isCorrect) {
+            core.setFailed(`Your branch is not allowed to PR into ${baseBranch}`);
             return
         }
 
 
     } catch (error) {
-        core.setFailed(error.menssage);
+        core.setFailed(error.message);
     }
 }
 
 
-const eventName = github.context.eventName;
-const {eventName, payload : {pull_request} } = github.context
 
-run(eventName,pull_request?.base.ref,pull_request?.head.ref);
+run();
